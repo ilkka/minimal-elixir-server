51a1a3744e9cafcc63ca3c512752e92a4de6d9ef
Ilkka Poutanen <ilkka@ilkka.io>
2018-10-13 09:26:50 +0300

    Implement stuff needed to spawn endpoint

--- lib/minserver/endpoint.ex
+++ lib/minserver/endpoint.ex
@@ -12,4 +12,15 @@ defmodule Minserver.Endpoint do
   match _ do
     send_resp(conn, 404, "Not found")
   end
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts) do
+    Plug.Adapters.Cowboy2.http(__MODULE__, [])
+  end
 end
